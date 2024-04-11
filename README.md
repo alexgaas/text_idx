6977ca0372a6a47e24df314f87b81ecf7a578510
Alex Gaas <alex.gaas@slalom.com>
2024-04-11 14:00:21 -0600

    Update feedback

--- README.md
+++ README.md
@@ -1,10 +1,44 @@
 # text_idx
+The project `text_idx` is effective _Java_-based implementation of:
+- sparse index by many structured files and effective search by index
+- log-structured merge tree (within structured string table and write ahead log)
 
-## Explanation
+### Explanation
 
-## Results
+### Results
+Developer API and results shown in the unit test 
+[LsmTest.java](https://github.com/alexgaas/text_idx/blob/4b60ce1ee2f15a0b2d309917737d63314628a58e/src/test/java/lsm/LsmTest.java)
 
-## TODO
+**Developer API** example:
+```text
+try(Store lsm = new Lsm(baseTestPath, 4, 3)) {
+    // put test data
+    for (int i1 = 0; i1 < 10; i1++) {
+        lsm.put(String.valueOf(i1), String.valueOf(i1));
+    }
+    // assert data in the LSM store
+    for (int i1 = 0; i1 < 10; i1++) {
+        assertEquals(String.valueOf(i1), lsm.get(String.valueOf(i1)));
+    }
+    // remove data from LSM store
+    for (int i1 = 0; i1 < 10; i1++) {
+        String s = String.valueOf(i1);
+        lsm.remove(s);
+    }
+    // assert data have been removed
+    for (int i = 0; i < 10; i++) {
+        String s = lsm.get(String.valueOf(i));
+        Assertions.assertNull(s);
+    }
+}
+```
 
-## License
+### TODO
+- `text_idx` uses `RandomAccessFile` for most of IO operations. This IO API in fact is
+deprecated and have to be replaced with `FileChannel`. Details in this article -
+  https://github.com/alexgaas/java_file_io
+- there is no `BloomFilter`
+- no effective benchmark tests
+
+### License
 MIT - https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt
--- src/test/java/lsm/LsmTest.java
+++ src/test/java/lsm/LsmTest.java
@@ -20,27 +20,25 @@ public class LsmTest {
 
     @Test
     public void basicLsmTest() throws IOException {
-        Store lsm = new Lsm(baseTestPath, 4, 3);
-
-        // put test data
-        for (int i1 = 0; i1 < 10; i1++) {
-            lsm.put(String.valueOf(i1), String.valueOf(i1));
+        try(Store lsm = new Lsm(baseTestPath, 4, 3)) {
+            // put test data
+            for (int i1 = 0; i1 < 10; i1++) {
+                lsm.put(String.valueOf(i1), String.valueOf(i1));
+            }
+            // assert data in the LSM store
+            for (int i1 = 0; i1 < 10; i1++) {
+                assertEquals(String.valueOf(i1), lsm.get(String.valueOf(i1)));
+            }
+            // remove data from LSM store
+            for (int i1 = 0; i1 < 10; i1++) {
+                String s = String.valueOf(i1);
+                lsm.remove(s);
+            }
+            // assert data have been removed
+            for (int i = 0; i < 10; i++) {
+                String s = lsm.get(String.valueOf(i));
+                Assertions.assertNull(s);
+            }
         }
-        // assert data in the LSM store
-        for (int i1 = 0; i1 < 10; i1++) {
-            assertEquals(String.valueOf(i1), lsm.get(String.valueOf(i1)));
-        }
-        // remove data from LSM store
-        for (int i1 = 0; i1 < 10; i1++) {
-            String s = String.valueOf(i1);
-            lsm.remove(s);
-        }
-        // assert data have been removed
-        for (int i = 0; i < 10; i++) {
-            String s = lsm.get(String.valueOf(i));
-            Assertions.assertNull(s);
-        }
-
-        lsm.close();
     }
 }
